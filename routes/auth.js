93b013c3b88db697e0bbb99030118f0e13f4bfe4
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2022-08-10 13:49:36 +0530

    install jwt  , create login and upadte

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const User = require("./models/User");
+
+//to prevent unotharized access
 const CryptoJS = require("crypto-js");
+
+//to use to do more secure in application
+const jwt = require("jsonwebtoken");
+
 
 
 //Register
 
-router.post("/register" , async (req,res) =>{
+router.post("/register", async (req, res) => {
     const newUser = new User({
         //id: req.body.id,
         firstName: req.body.firstName,
@@ -16,20 +22,58 @@ router.post("/register" , async (req,res) =>{
         status: req.body.status,
 
         //to prevent unotharized access
-        password : CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
-        .toString(),
-        accountType : req.body.accountType,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
+            .toString(),
+        accountType: req.body.accountType,
     });
-     // using try catch block to catch problem in our server or db
-    try{
+    // using try catch block to catch problem in our server or db
+    try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser)
-    }catch(err){
-       res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
 //Login
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        //if user is not in system
+        !user && res.status(401).json("wrong credentials!");
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password,
+            process.env.PASS_SEC
+        );
+        // use jwt 
+        const accessToken = jwt.sign({
+            id:user._id,
+            accountType: user.accountType,
+        },
+        process.env.JWT_SEC,
+        {
+            expiresIn: "10d"
+        }
+        );
+        const pssword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        if (pssword !== req.body.password) {
+            res.status(401).json("wrong credentials!");
+        } else {
+            const { password, ...others } = user._doc;
+            res.status(200).json({...others, accessToken});
+
+        }
+
+       
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,8 @@
+const User = require("./models/User");
+const {verifyToken,verifyTokenAndAuthorization} = require("./verifyToken");
+
 const router = require("express").Router();
+
 
 //get user input and after response it.(req,res)
 /*router.get("/usertest", (req,res) => {
@@ -14,7 +18,28 @@ router.post("/userposttest", (req,res) => {
     res.send("your username is:" + username);
 })*/
 
+//UPDATE
+router.put("/:id" , verifyTokenAndAuthorization , async (req,res) => {
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(
+           req.body.password,
+           process.env.PASS_SEC
 
+        ).toString();
+    }
+    try
+   { 
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+
+    },
+     {new:true}
+
+     );
+}catch(err){
+   res.status(500).json(err);
+}
+});
 
 
 
